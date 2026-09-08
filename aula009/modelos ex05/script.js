ad1e8903ccd4284af4b261df976b124cc8a3848e
José Miguel Atutu <businesscooperation158@gmail.com>
2026-09-08 18:51:40 +0100

    js

--- aula009/modelos ex05/script.js
+++ aula009/modelos ex05/script.js
@@ -1,1 +1,23 @@
-alert('Ola')
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    // var hora = data.getHours()
+    var hora = 19
+    msg.innerHTML = `Agora são ${hora} horas`
+    if(hora >= 0 && hora < 12){
+        // Bom dia
+        img.src = 'manha.png'
+        document.body.style.background = '#d6dbef'
+        
+    }else if(hora >= 12 && hora < 18){
+        // Boa tarde
+        img.src = 'tarde.png'
+        document.body.style.background = '#ffd579'
+    }else{
+        // Boa noite
+        img.src = 'noite.png'
+        document.body.style.background = '#211c19'
+    
+    }
+}
