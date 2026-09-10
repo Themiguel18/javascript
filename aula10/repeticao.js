889553584c62ef7eb04f9fd1c8130f3cc2fea8f4
José Miguel Atutu <businesscooperation158@gmail.com>
2026-09-10 16:33:08 +0100

    js

---
+++ aula10/repeticao.js
@@ -0,0 +1,17 @@
+var c = 1
+do{
+    console.log('Tudo bem')
+    c++
+}while(c <= 10)
+
+
+
+
+
+
+/*console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')*/
