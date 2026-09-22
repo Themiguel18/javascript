797645198b4f7a34064dd1de07b4b2eb8728dcea
José Miguel Atutu <businesscooperation158@gmail.com>
2026-09-22 15:04:22 +0100

    js

---
+++ desafios/D01/script.js
@@ -0,0 +1,33 @@
+let numeros = document.getElementById('numeros')
+let res = document.getElementById('res')
+let txt = document.getElementById('txt')
+function analisar(){
+    if(numeros.value.length == 0){
+        alert('Digite uma cena palhaço')
+        return
+    }else{
+    let valor = Number(numeros.value)
+    let item = document.createElement('option')
+    item.textContent = valor
+    res.appendChild(item)
+
+    }
+
+
+    
+}
+
+function resultado(){
+     let r = 0
+    for(let c of res.options){
+        let numero = Number(c.textContent)
+        r += numero
+        
+
+    }txt.innerHTML = `A soma de todos e ${r}`
+
+    let maior = res.options
+    if(maior < res.options){
+        maior = res.options
+    }txt.innerHTML = `O maior numero e ${maior}`
+}
