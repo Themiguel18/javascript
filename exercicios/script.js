f26cdce552829f0b665c0cc07f3ea5c43c3eab77
José Miguel Atutu <businesscooperation158@gmail.com>
2026-09-23 21:20:18 +0100

    js

--- exercicios/script.js
+++ exercicios/script.js
@@ -1,11 +1,23 @@
-function mostrar(){
-    let nome = document.getElementById('txt')
-    let texto = document.getElementById('txt1')
-   
-    if(nome.value.length == 0){
-        alert('Digite um nome seu desgraçado')
+let numero = document.getElementById('numero')
+let lista = document.getElementById('lista')
+let res = document.getElementById('res')
+function mostreMenor(){
+    if(numero.value.length == 0){
+        alert('Digite um valor seu palhaço')
     }else{
-        let n = String(nome.value)
-        texto.innerHTML = 'Ola '+ n
+        let valor = Number(numero.value)
+        let item = document.createElement('option')
+        item.textContent = valor
+        lista.appendChild(item)
+        let menor = Infinity
+        for(let p of lista.options){
+            let v = Number(p.value)
+            if(v < menor){
+                menor = v
+            }
+        }res.innerHTML = `O menor número digitado é ${menor}`
+
+
+
     }
 }
