227c703b5a8a940294ad50e59f3fa761cd994153
José Miguel Atutu <businesscooperation158@gmail.com>
2026-09-24 15:18:01 +0100

    js

---
+++ exercicios/arrow.js
@@ -0,0 +1,13 @@
+const par = n => {
+    
+    if(n%2==0){
+        console.log(n + ' e par')
+        return n
+    }else{
+        console.log(n + ' e impar')
+        return n
+    }
+    
+} 
+console.log(par(3))
+
