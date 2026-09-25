35b652f08cddb0d078a8b47c66695403b13e1bc8
José Miguel Atutu <businesscooperation158@gmail.com>
2026-09-25 17:56:59 +0100

    js

--- ex22.js
+++ ex22.js
@@ -1,4 +1,25 @@
-let somar = (v1, v2) => {
-    return v1+v2
+function multiplicar(n){
+    n = 3
+    let m = n
+    return function multiplica(){
+        let mult = m*5
+        return mult
+    }    
+    
 }
-console.log(somar(2,2))
+
+let multiplicador = multiplicar()
+console.log(multiplicador())
+
+
+
+// function criarContador(){
+//     let c = 0
+//     return function cn(){
+//         c++
+//         return c
+        
+//     }
+// }
+// let contador = criarContador()
+// console.log(contador())
