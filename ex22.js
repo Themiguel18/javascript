b5302c05151a5b348440c55c7d0f27be4e1cc151
José Miguel Atutu <businesscooperation158@gmail.com>
2026-09-25 18:44:20 +0100

    js

--- ex22.js
+++ ex22.js
@@ -1,25 +1,51 @@
-function multiplicar(n){
-    n = 3
-    let m = n
-    return function multiplica(){
-        let mult = m*5
-        return mult
+function criarConta(saldoInicial){
+    let guadarSaldo = saldoInicial
+    return {
+        depositar(valor){
+            guadarSaldo += valor
+             return guadarSaldo
+        },
+
+        verSaldo(){
+            return guadarSaldo
+        }
+    }
+}
+
+let conta = criarConta(500)
+console.log(conta.verSaldo())
+console.log(conta.depositar(200))
+console.log(conta.depositar(200))
+console.log(conta.verSaldo())
+
+
+
+
+
+
+/*function multiplicar(n){
+
+
+
+    return function multiplica(n1){
+       
+        return n*n1
     }    
     
 }
 
-let multiplicador = multiplicar()
-console.log(multiplicador())
+let multiplicador = multiplicar(3)
+console.log(multiplicador(3))
 
 
 
-// function criarContador(){
-//     let c = 0
-//     return function cn(){
-//         c++
-//         return c
+ function criarContador(){
+   let c = 0
+     return function cn(){
+         c++
+         return c
         
-//     }
-// }
-// let contador = criarContador()
-// console.log(contador())
+     }
+ }
+ let contador = criarContador()
+ console.log(contador())*/
